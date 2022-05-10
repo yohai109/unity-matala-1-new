022eca8c9270be178c75cad2da6ab0daa4fc08e2
Yoav Zinger <47221812y@gmail.com>
2022-05-10 19:41:03 +0300

    finished main menu

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainMenuScript : MonoBehaviour
+{
+    //public Canvas quitMenu;
+    public Button starText;
+    public Button exitText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ExitPress() {
+        //quitMenu.enabled = true;
+        starText.enabled = false;
+        exitText.enabled = false;
+        
+    }
+
+public void NoPress() {
+        // quitMenu.enabled = false;
+        starText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void StartLevel () {
+        SceneManager.LoadScene("Game");
+    }
+
+
+    public void ExitGame (){
+        Application.Quit();
+    }
+}
